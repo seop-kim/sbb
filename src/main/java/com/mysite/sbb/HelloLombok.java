a99ee836f7580fb2e42b06a47aa3ea025a249e25
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-14 16:58:07 +0900

    feat: Add constructor

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -10,10 +10,14 @@ public class HelloLombok {
     private String hello;
     private int lombok;
 
+    public HelloLombok(String hello, int lombok) {
+        this.hello = hello;
+        this.lombok = lombok;
+    }
+
+
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("Hello Lombok!");
-        helloLombok.setLombok(100);
+        HelloLombok helloLombok = new HelloLombok("Hello Lombok!",100);
 
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
