4e554469dedd724735debce251f420653c95eccb
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-22 16:20:06 +0900

    feat: Add controller model
    Pass data from the controller to the front using the model

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,13 +1,23 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionRepository questionRepository;
 
+    // ========== Question 목록 ==========
     @GetMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+        // Question 전체 조회를 하여 model에 저장하여 front로 전달한다.
+        List<Question> questions = questionRepository.findAll();
+        model.addAttribute("questions", questions);
+
         return "question_list";
     }
 }
