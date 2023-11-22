639fde88687f9a4214b6af81ec35ac6d60bb4e5a
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-22 16:07:34 +0900

    test: Add entity test

--- src/test/java/com/mysite/sbb/QuestionRepositoryTest.java
+++ src/test/java/com/mysite/sbb/QuestionRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.mysite.sbb;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +34,11 @@ class QuestionRepositoryTest {
     }
 
     @Test
+    void saveClear() {
+        questionRepository.deleteAll();
+    }
+
+    @Test
     void findAllTest() {
         List<Question> questions = this.questionRepository.findAll();
         assertThat(questions).hasSize(2);
@@ -44,7 +51,7 @@ class QuestionRepositoryTest {
     @Test
     void findByIdTest() {
         Optional<Question> question1 = this.questionRepository.findById(2);
-        if (question1.isPresent()){
+        if (question1.isPresent()) {
             Question question = question1.get();
             assertThat(question.getSubject())
                     .isEqualTo("스프링부트 모델 질문입니다.");
@@ -54,8 +61,64 @@ class QuestionRepositoryTest {
     @Test
     void findBySubject() {
         Question question = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+
         assertThat(question.getId())
                 .isEqualTo(1);
     }
+
+    @Test
+    void findBySubjectAndContentTest() {
+        Question question =
+                this.questionRepository.findBySubjectAndContent(
+                        "sbb가 무엇인가요?",
+                        "sbb에 대해 알고 싶습니다.");
+
+        assertThat(question.getId())
+                .isEqualTo(1);
+    }
+
+    @Test
+    void findBySubjectLikeTest() {
+        List<Question> questions = this.questionRepository.findBySubjectLike("sbb%");
+        Question question = questions.get(0);
+
+        assertThat(question.getSubject())
+                .isEqualTo("sbb가 무엇인가요?");
+    }
+
+    @Test
+    void updateTest() {
+        Optional<Question> opFindOne = this.questionRepository.findById(1);
+        assertTrue(opFindOne.isPresent());
+
+        Question question = opFindOne.get();
+        question.setSubject("수정된 제목");
+        this.questionRepository.save(question);
+    }
+
+    @Test
+    void deleteTest() {
+        // 최초에 2개의 데이터
+        assertThat(questionRepository.count())
+                .isEqualTo(2);
+
+        // 1번 아이디를 가진 데이터를 가져오다
+        Optional<Question> findOne = questionRepository.findById(1);
+        assertThat(findOne)
+                .isPresent();
+
+        Question question = findOne.get();
+
+        // 1번 아이디 데이터 삭제
+        questionRepository.delete(question);
+
+        // 2개 중 1개의 삭제로 결과는 1
+        assertThat(questionRepository.count())
+                .isEqualTo(1);
+    }
+
+
+
+
 
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,12 +1,74 @@
 package com.mysite.sbb;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
 
     @Test
-    void contextLoads() {
+    void answerCreateTest() {
+        Optional<Question> questionFindOne = questionRepository.findById(2);
+        assertThat(questionFindOne)
+                .isPresent();
+
+        Question question = questionFindOne.get();
+
+        Answer answer = new Answer();
+        answer.setContent("네 자동으로 생성됩니다.");
+        answer.setQuestion(question);
+        answer.setCreateDate(LocalDateTime.now());
+        answerRepository.save(answer);
+    }
+
+    @Test
+    @DisplayName("1번 answer의 question id는 2다")
+    void answerInQuestionIdTest() {
+        Optional<Answer> answerFindOne = answerRepository.findById(1);
+        assertThat(answerFindOne)
+                .isPresent();
+
+        Answer answer = answerFindOne.get();
+
+        assertThat(answer.getQuestion().getId())
+                .isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("1번 질문에 있는 답변 수가 1이 맞는가? / 답변의 내용이 네 '자동으로 생성됩니다.'와 일치하는가?")
+    @Transactional
+    void questionInAnswersFindTest() {
+        // Transactional 어노테이션을 사용하지 않을 경우 첫번째 question 을 조회한 뒤 DB세션이 종료되어 뒤에 코드에서 오류가 발생한다.
+
+        Optional<Question> questionFindOne = questionRepository.findById(2);
+        assertThat(questionFindOne)
+                .isPresent();
+
+        Question question = questionFindOne.get();
+
+        List<Answer> answers = question.getAnswers();
+
+        assertThat(answers.size())
+                .isEqualTo(1);
+
+        assertThat(answers.get(0).getContent())
+                .isEqualTo("네 자동으로 생성됩니다.");
     }
 }
