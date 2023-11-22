dc9e2462150176033af0873338f64437e3a0cdfc
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-22 22:37:17 +0900

    feat: Add comment

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -4,7 +4,6 @@ import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +16,13 @@ public class AnswerController {
     private final AnswerService answerService;
     private final QuestionService questionService;
 
+    // ========== 답변 저장 ==========
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
+    public String createAnswer(@PathVariable Integer id, @RequestParam String content) {
         Question question = questionService.getQuestion(id);
 
         // TODO : Answer save
-
+        answerService.create(question, content);
 
         return "redirect:/question/detail/%s".formatted(id);
     }
