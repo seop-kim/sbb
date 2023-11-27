e24619a5078cd76a7f784eb772e49d448e4f8415
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-27 21:15:28 +0900

    refactor: Add error responsible html

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,12 +2,14 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,12 +20,16 @@ public class AnswerController {
 
     // ========== 답변 저장 ==========
     @PostMapping("/create/{id}")
-    public String createAnswer(@PathVariable Integer id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.getQuestion(id);
 
         // TODO : Answer Save
-        answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
 
+        answerService.create(question, answerForm.getContent());
         return "redirect:/question/detail/%s".formatted(id);
     }
 }
