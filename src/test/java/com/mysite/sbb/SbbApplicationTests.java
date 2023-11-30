7ba5f222127b1131202e5fb4db473cce75b76742
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-30 22:44:29 +0900

    feat: Add spring security

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -6,6 +6,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -23,9 +25,25 @@ class SbbApplicationTests {
     @Autowired
     private AnswerRepository answerRepository;
 
+    @Autowired
+    private QuestionService questionService;
+
+
     @Test
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = "테스트 데이터입니다 : %03d".formatted(i);
+            String content = "내용무";
+            questionService.create(subject, content);
+        }
+    }
+
+    @DisplayName("[답변 등록] 답변등록 시 db에는 동일한 데이터가 저장되어야 한다.")
+    @Test
+    @Rollback
     void answerCreateTest() {
         Optional<Question> questionFindOne = questionRepository.findById(2);
+
         assertThat(questionFindOne)
                 .isPresent();
 
@@ -35,11 +53,14 @@ class SbbApplicationTests {
         answer.setContent("네 자동으로 생성됩니다.");
         answer.setQuestion(question);
         answer.setCreateDate(LocalDateTime.now());
-        answerRepository.save(answer);
+        Answer saveAnswer = answerRepository.save(answer);
+
+        assertThat(answer)
+                .isEqualTo(saveAnswer);
     }
 
     @Test
-    @DisplayName("1번 answer의 question id는 2다")
+    @DisplayName("[답변 확인] 1번 answer의 question id는 2다")
     void answerInQuestionIdTest() {
         Optional<Answer> answerFindOne = answerRepository.findById(1);
         assertThat(answerFindOne)
@@ -52,12 +73,12 @@ class SbbApplicationTests {
     }
 
     @Test
-    @DisplayName("1번 질문에 있는 답변 수가 1이 맞는가? / 답변의 내용이 네 '자동으로 생성됩니다.'와 일치하는가?")
+    @DisplayName("[답변 확인] 3번 질문에 있는 답변 수가 0이 맞는가?")
     @Transactional
     void questionInAnswersFindTest() {
         // Transactional 어노테이션을 사용하지 않을 경우 첫번째 question 을 조회한 뒤 DB세션이 종료되어 뒤에 코드에서 오류가 발생한다.
 
-        Optional<Question> questionFindOne = questionRepository.findById(2);
+        Optional<Question> questionFindOne = questionRepository.findById(3);
         assertThat(questionFindOne)
                 .isPresent();
 
@@ -66,9 +87,6 @@ class SbbApplicationTests {
         List<Answer> answers = question.getAnswers();
 
         assertThat(answers.size())
-                .isEqualTo(1);
-
-        assertThat(answers.get(0).getContent())
-                .isEqualTo("네 자동으로 생성됩니다.");
+                .isEqualTo(0);
     }
 }
