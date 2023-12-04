87feef9fff43a44ebf0ae792d0a615f74640547d
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-04 19:26:59 +0900

    refactor: Apply builder annotation

--- src/main/java/com/mysite/sbb/user/SiteUser.java
+++ src/main/java/com/mysite/sbb/user/SiteUser.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@Setter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class SiteUser {
     @Id
@@ -24,4 +26,11 @@ public class SiteUser {
     @Column(unique = true)
     private String email;
 
+
+    @Builder
+    public SiteUser(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
