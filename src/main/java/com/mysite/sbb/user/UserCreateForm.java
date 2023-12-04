151e32c015034543a1d1447c2fea763c6ebf6b9e
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-04 19:42:41 +0900

    feat: Add userController

---
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateForm {
+    @Size(min = 3, max = 25)
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password1;
+
+    @NotEmpty
+    private String password2;
+
+    @NotEmpty
+    @Email
+    private String email;
+}
