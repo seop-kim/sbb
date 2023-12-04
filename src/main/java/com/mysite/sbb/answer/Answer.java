d2f8ba3fbba0fd5d54340902d9b818675d3a3475
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-04 20:43:31 +0900

    feat: Add author column in answer class

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,6 +19,9 @@ public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne
+    private SiteUser author;
 
     @Column(columnDefinition = "TEXT")
     private String content;
