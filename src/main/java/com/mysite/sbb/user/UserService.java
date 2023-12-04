462ec1da20b4aacd60a6803983eaeba40eb23e5c
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-04 19:42:28 +0900

    refactor: Modify passwordEncoder as a global variable

--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,16 +1,16 @@
 package com.mysite.sbb.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String mail, String password) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         SiteUser siteUser = SiteUser.builder()
                 .username(username)
                 .email(mail)
